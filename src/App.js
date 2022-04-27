5cabd92ad4df04143ca8011de239aab924ef4323
beth98an <bke.evans@gmail.com>
2022-04-27 10:34:50 +0100

    search form for by user added to components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import { ByUser } from './components'
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+    <ByUser/>
+    </>
   );
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export {ByUser} from './ByUser';
